d1bfe4f4e24632aafb6d9530ab3ef8cd902275f5
ManatoYamashita <info@manapuraza.com>
2025-05-28 09:33:20 +0900

    FIX: test error for bookmarklet to encode inline

--- test.js
+++ test.js
@@ -1,11 +1,1 @@
-javascript:(function() {
-    // ブックマークレットが実行されたことを示すアラート
-    alert('Hello from Bookmarklet!');
-
-    // 現在のページのタイトルとURLをコンソールに表示
-    console.log('Page Title:', document.title);
-    console.log('Page URL:', window.location.href);
-
-    // 指定のURLにリダイレクト
-    window.location.href = 'https://manapuraza.com';
-})();
+javascript:(function(){alert('Hello from Bookmarklet!');console.log('Page Title:',document.title);console.log('Page URL:',window.location.href);window.location.href='https://manapuraza.com';})();
